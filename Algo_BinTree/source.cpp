23795c43b07ad876c56c0589ae883d8e8a28e2b9
PJW <pmnxis@live.co.kr>
2015-11-06 20:42:35 +0900

    renew data

--- Algo_BinTree/source.cpp
+++ Algo_BinTree/source.cpp
@@ -36,7 +36,7 @@ struct doublelist {
 
 void insertNode(node * root, char input[]);
 
-bool deleteNode(node * target, char input[]);
+bool deleteNode(node * pretarget, node * target, char input[]);
 
 node * mkNode(bool type);
 
@@ -51,7 +51,7 @@ void postorder_tranversal(node * target);
 
 bool tellType(node * target);
 
-bool findEmptyNil(node * target);
+bool detectNilKeyProblem(node * target);
 
 void removeTree(node * target);
 
@@ -70,7 +70,7 @@ int main() {
 			insertNode(root, &cstrBuff[1]);
 		}
 		else if (cstrBuff[0] == '-') {
-			deleteNode(root, &cstrBuff[1]);
+			deleteNode(NULL, root, &cstrBuff[1]);
 		}
 		else {
 			goto exception;
@@ -119,45 +119,77 @@ void insertNode(node * target, char input[]) {
 	return;
 }
 
-bool deleteNode(node * target, char input[])
+bool deleteNode(node * pretarget, node * target, char input[])
 {
 	bool listener = 0;
-
-	if (target->nodeType == key)listener = 1;
+	int or01 = -1;
 
 	if (input[0] == '0') {
-		if (target->zero != NULL)listener = deleteNode(target->zero, &input[1]) || listener;
+		printf("0");
+		or01 = 0;
+		if (target->zero != NULL) { listener = deleteNode(target, target->zero, &input[1]) || listener; }
 		else goto exception;
 	}
 	else if (input[0] == '1') {
-		if (target->one != NULL)listener = deleteNode(target->one, &input[1]) || listener;
+		printf("1");
+		or01 = 1;
+		if (target->one != NULL)listener = deleteNode(target, target->one, &input[1]) || listener;
 		else goto exception;
 	}
 	else {
-		if (target->nodeType == nil) {
+		printf(" ]- ");
+		if (target->zero == NULL && target->one == NULL) {
+			printf(" nil removed\n");
 			free(target);
-			return 0;
+			//   만약 1이 리턴되면 이제 그앞 재귀프로세서는 후삭제 확인을 해야한다고 알려주는것이다.
+			//  만약 0이 리턴되면 그럴필요없이 끝내도 된다.
+			return 1;
 		}
 		else if (target->nodeType == key) {
-			return 1;
+			//free(target);
+			return 0;
 		}
 		else {
 			goto exception;
-			return 1;
+			return 0;
 		}
 	}
 
-	if (listener == 0) {
-		free(target);
-		return 0;
+	if (listener == 0)return 0;
+	else if (listener == 1) {
+		// after get returned 
+		if (or01 == 0) {
+			if (pretarget == NULL) {
+				goto exception;
+			}
+			else {
+				if (pretarget->one == NULL) {
+					free(target);
+					pretarget->zero = NULL;
+					return 1;
+				}
+			}
+		}
+		else if (or01 == 1) {
+			if (pretarget == NULL) {
+				goto exception;
+			}
+			else {
+				if (pretarget->zero == NULL) {
+					free(target);
+					pretarget->one = NULL;
+					return 1;
+				}
+			}
+
+		}
 	}
-	else {
-		return 1;
-	}
+
+
 
 exception:
 	printf("delNode Func : Error occured : %s, looper\n", input);
-	return 1;
+	return 0;
 }
 
 node * mkNode(bool type) {
@@ -201,22 +233,21 @@ bool tellType(node * target) {
 	return target->nodeType;
 }
 
-bool findEmptyNil(node * target) {
+bool detectNilKeyProblem(node * target) {
+	bool t0 = 0, t1 = 0;
 	if (target->one == NULL && target->zero == NULL) {
-		if (target->nodeType == nil)return 0;
-		else return 1;
-
+		if (target->nodeType == nil)return 1;
+		else return 0;
 	}
 
-	if (findEmptyNil(target) == nil)return 0;
 	if (target->zero != NULL) {
-		if (findEmptyNil(target->zero) == nil)return 0;
+		if (target->zero->nodeType == key)t1 =
+			t0 = detectNilKeyProblem(target->zero);
 	}
 	if (target->one != NULL) {
-		if (findEmptyNil(target->one) == nil)return 0;
+		t1 = detectNilKeyProblem(target->one);
 	}
-
-	else return 1;
+	return (t0 || t1);
 }
 
 void removeTree(node * target) {
